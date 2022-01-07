20ed2ca677d1482e0994c0a257f6484820f8c697
米鸿坤 <1045262335@qq.com>
2022-01-07 06:01:14 +0000

    feat: add function add(int, int)

--- src/hello.c
+++ src/hello.c
@@ -1,9 +1,19 @@
 # include <stdio.h>
 
+// 声明
+int add(int, int);
 
 // 入口
 int main() {
-    printf("hello world");
+    printf("hello world\n");
+    int a = 1;
+    int b = 2;
+    int c = add(a,b);
+    printf("c = %d\n", c);
     return 0;
 }
 
+// 两整数相加
+int add(int a, int b) {
+    return a + b;
+}
